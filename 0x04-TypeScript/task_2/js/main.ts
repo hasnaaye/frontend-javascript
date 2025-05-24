bb208af1f900473a2e9bfbccdfdd4d5aeb394dc8
hasnaaye <hasnaeyerrou@gmail.com>
2025-05-25 00:05:11 +0100

    Update main.ts

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,6 +1,59 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface{  
+  workFromHome(): string {
+    return "Working from home";
+  }
+  getCoffeeBreak(): string {
+    return "Getting a coffee break";
+  }
+  workDirectorTasks(): string{
+    return "Getting to director tasks";
+  }
+}
+class implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+  workDirectorTasks(): string{
+    return "Getting to work";
+  }
+}
+
+function createEmployee(salary: number | string):  Director | Teacher{
+  if (typeof salary === "number" && salary < 500){
+    return new Teacher(); 
+  }else {
+    return new Director();
+  }
+}
 console.log(createEmployee(200));
-Teacher
+//Teacher
 console.log(createEmployee(1000));
-Director
+//Director
 console.log(createEmployee('$500'));
-Director
+//Director
+
+function isDirector (employee): Director | Teacher{
+  
+}
+function executeWork(employee: Director | Teacher): string{
+  if (employee){}
+}
+executeWork(createEmployee(200));
+//Getting to work
+executeWork(createEmployee(1000));
+//Getting to director tasks
