9808c4a651a4d300c538ed58473bef7a6ed26a25
hasnaaye <hasnaeyerrou@gmail.com>
2025-05-25 00:54:20 +0100

    Update main.ts

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,1 +1,25 @@
+interface MajorCredits {
+  credits: number;
+  __brand: 'major';
+}
 
+interface MinorCredits {
+  credits: number;
+  __brand: 'minor';
+}
+
+// Function to sum MajorCredits
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    __brand: 'major'
+  };
+}
+
+// Function to sum MinorCredits
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    __brand: 'minor'
+  };
+}
