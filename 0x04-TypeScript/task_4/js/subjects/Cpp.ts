b5ebb6297a18cc37f87079bb3cf11c9d33704154
hasnaaye <hasnaeyerrou@gmail.com>
2025-05-25 00:50:18 +0100

    Update Cpp.ts

--- 0x04-TypeScript/task_4/js/subjects/Cpp.ts
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -1,1 +1,35 @@
+namespace Subjects {
+  export interface Teacher {
+    firstName: string;
+    lastName: string;
+    experienceTeaching?: number;
+  }
+}
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingC?: number;
+  }
+}
 
+namespace Subjects {
+  export class Subject {
+    protected teacher: Teacher;
+
+    set setTeacher(teacher: Teacher) {
+      this.teacher = teacher;
+    }
+  }
+
+  export class Cpp extends Subject {
+    getRequirements(): string {
+      return "Here is the list of requirements for Cpp";
+    }
+
+    getAvailableTeacher(): string {
+      if (!this.teacher || this.teacher.experienceTeachingC === undefined || this.teacher.experienceTeachingC <= 0) {
+        return "No available teacher";
+      }
+      return `Available Teacher: ${this.teacher.firstName}`;
+    }
+  }
+}
