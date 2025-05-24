9d995840d369c18f103a282bc5499476522078f7
hasnaaye <hasnaeyerrou@gmail.com>
2025-05-25 00:48:45 +0100

    Update Subject.ts

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,1 +1,14 @@
+export namespace Subjects {
+  export interface Teacher{
+    firstName: string;
+    lastName: string;
+  }
 
+  export class Subject {
+    private teacher: Teacher;
+
+    set setTeacher(teacher: Teacher) {
+      this.teacher = teacher;
+    }
+  }
+}
