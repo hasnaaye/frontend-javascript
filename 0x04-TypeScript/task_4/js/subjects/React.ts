794892b9d333f36b1c963c57fc78ad1d502b8b88
hasnaaye <hasnaeyerrou@gmail.com>
2025-05-25 00:51:26 +0100

    Update React.ts

--- 0x04-TypeScript/task_4/js/subjects/React.ts
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -1,1 +1,25 @@
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingReact?: number;
+  }
+}
 
+namespace Subjects {
+  // ✅ React class extending Subject
+  export class React extends Subject {
+    getRequirements(): string {
+      return "Here is the list of requirements for React";
+    }
+
+    getAvailableTeacher(): string {
+      if (
+        !this.teacher ||
+        this.teacher.experienceTeachingReact === undefined ||
+        this.teacher.experienceTeachingReact <= 0
+      ) {
+        return "No available teacher";
+      }
+      return `Available Teacher: ${this.teacher.firstName}`;
+    }
+  }
+}
